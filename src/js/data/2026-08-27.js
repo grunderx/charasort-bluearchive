be20a8fcc0ce5ae31c06761ce90f1a08768a5293
GrunderX <grunderx@gmail.com>
2026-09-25 15:17:06 +0700

    Fix dataSetVersion for previous datasets

--- src/js/data/2026-08-27.js
+++ src/js/data/2026-08-27.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2026-06-22"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2026-08-27"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
